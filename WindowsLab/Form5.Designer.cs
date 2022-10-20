5638f768aba6ccddc6b37cf13cf51c03c6ea9093
Mariela Valentini <marielavalentini@outlook.com>
2022-10-20 18:03:13 -0300

    Ejercicios

---
+++ WindowsLab/Form5.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace WindowsLab
+{
+    partial class Form5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblSexo = new System.Windows.Forms.Label();
+            this.lblTipoUsuario = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtSexo = new System.Windows.Forms.TextBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(50, 33);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(118, 16);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Ingrese su nombre";
+            // 
+            // lblSexo
+            // 
+            this.lblSexo.AutoSize = true;
+            this.lblSexo.Location = new System.Drawing.Point(50, 72);
+            this.lblSexo.Name = "lblSexo";
+            this.lblSexo.Size = new System.Drawing.Size(101, 16);
+            this.lblSexo.TabIndex = 1;
+            this.lblSexo.Text = "Ingrese su sexo";
+            // 
+            // lblTipoUsuario
+            // 
+            this.lblTipoUsuario.AutoSize = true;
+            this.lblTipoUsuario.Location = new System.Drawing.Point(50, 108);
+            this.lblTipoUsuario.Name = "lblTipoUsuario";
+            this.lblTipoUsuario.Size = new System.Drawing.Size(160, 16);
+            this.lblTipoUsuario.TabIndex = 2;
+            this.lblTipoUsuario.Text = "Ingrese su tipo de usuario";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(235, 30);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 22);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // txtSexo
+            // 
+            this.txtSexo.Location = new System.Drawing.Point(235, 66);
+            this.txtSexo.Name = "txtSexo";
+            this.txtSexo.Size = new System.Drawing.Size(100, 22);
+            this.txtSexo.TabIndex = 4;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(235, 108);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(100, 22);
+            this.txtUsuario.TabIndex = 5;
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(260, 166);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(75, 23);
+            this.btnMostrar.TabIndex = 6;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // Form5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.txtSexo);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblTipoUsuario);
+            this.Controls.Add(this.lblSexo);
+            this.Controls.Add(this.lblNombre);
+            this.Name = "Form5";
+            this.Text = "Form5";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblSexo;
+        private System.Windows.Forms.Label lblTipoUsuario;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtSexo;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Button btnMostrar;
+    }
+}
---
+++ WindowsLab/Form5.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsLab
+{
+    public partial class Form5 : Form
+    {
+        public Form5()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            string sexo = txtSexo.Text; 
+            string rol = txtUsuario.Text;
+            validarRol(rol);
+            validarSexo(sexo);
+        }
+
+        #region MyRegion
+
+        public void validarRol (string rol)
+        {
+            if (rol != "administrador" && rol != "estandar" && rol != "invitado")
+            {
+                MessageBox.Show("EL ROL INGRESADO NO ESTA HABILITADO, INGRESE ADMINISTRADOR, ESTANDAR O INVITADO");
+            }
+        }
+        public void validarSexo(string sexo)
+        {
+            if( sexo != femenino && sexo != masculino)
+            {
+                MessageBox.Show("EL SEXO INGRESADO ES INCORRECTO, POR FAVOR INGRESAR FEMENINO O MASCULINO");
+
+            }
+        }
+
+        #endregion
+    }
+}
